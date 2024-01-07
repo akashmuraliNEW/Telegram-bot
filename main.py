f5960928d782e868584401981276b8dcf9756b90
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2024-01-07 21:05:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 Dbclient = AsyncIOMotorClient('mongodb+srv://spotify:spotify@cluster0.tmcsezs.mongodb.net/?retryWrites=true&w=majority')
 Cluster = Dbclient['Cluster0']
 Data = Cluster['users']
-
+ADMINS = int('823882413')
 #Enter your bot credentials
 api_id = 8192282
 api_hash = '990a85e4f02364ddf5927728e75450b5'
@@ -263,6 +263,45 @@ async def start_command(client, message):
     user_id = message.from_user.id   
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id}) 
     await message.reply_text('**Hello, I am a Music downloader bot.Use /s or /song {song_name} To download songs,will be adding download support for other social medias(insta,twitter,tiktok,fb...)')
+
+@app.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))  
+async def broadcast(c, m):
+    if m.text == "/users":
+        total_users = await Data.count_documents({})
+        return await m.reply(f"Total Users: {total_users}")
+    b_msg = m.reply_to_message
+    sts = await m.reply_text("Broadcasting your messages...")
+    users = Data.find({})
+    total_users = await Data.count_documents({})
+    done = 0
+    failed = 0
+    success = 0
+    start_time = time.time()
+    async for user in users:
+        user_id = int(user['id'])
+        try:
+            await b_msg.copy(chat_id=user_id)
+            success += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await b_msg.copy(chat_id=user_id)
+            success += 1
+        except InputUserDeactivated:
+            await Data.delete_many({'id': user_id})
+            failed += 1
+        except UserIsBlocked:
+            failed += 1
+        except PeerIdInvalid:
+            await Data.delete_many({'id': user_id})
+            failed += 1
+        except Exception as e:
+            failed += 1
+        done += 1
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.delete()
+    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
 
 
 @app.on_message(filters.command("general"))
